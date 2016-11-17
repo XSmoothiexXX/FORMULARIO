c8d4ecfcac2606d5983ffd681b09722d7d55fdb7
enrique <ecs1997@gmail.com>
2016-11-17 16:49:40 +0100

    CREAMOS UN PROYECTO FORMULARIO PRACTICA LMAO

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Edad: <input type="text" name="edad">
+	Ciudad:
+	<select name="ciudad">
+		<option value="Barcelona"></option>
+		<option value="Girona"></option>
+		<option value="Lleida"></option>
+		<option value="Tarragona"></option>
+	</select>
+	Sexo:
+	Hombre: <input type="radio" name="sex" value="hombre">
+	Mujer: <input type="radio" name="sex" value="mujer">
+    </body>
+</html>
